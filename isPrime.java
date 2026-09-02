a22b75ac4c0187d24c05fca87a6238c5d820b32a
Aakash Maurya <aakash17x@gmail.com>
2026-09-02 09:06:05 +0530

    Implement isPrime method to check prime numbers

---
+++ isPrime.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPrime(int n) {
+        // O(sqrt(n))
+          int count = 0;
+          for(int i=1;i*i<=n;i++){
+            if(n%i ==0){
+                count++;
+                if((n/i) != i) count++;
+            }
+          }
+          return count == 2;
+    }
+}
